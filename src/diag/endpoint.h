1740d76da4e469ea9612b577417c5cb78e793236
Ashcon Mohseninia <ashconm@outlook.com>
2022-01-26 22:44:21 +0000

    Add USB Tx/Rx Diag data

--- src/diag/endpoint.h
+++ src/diag/endpoint.h
@@ -1,9 +1,15 @@
-#ifndef DIAG_ENDPOINT_H__
-#define DIAG_ENDPOINT_H__
+#ifndef _DIAG_ENDPOINT_H__
+#define _DIAG_ENDPOINT_H__
 
 #include <stdint.h>
+#include <driver/uart.h>
+#include "esp_log.h"
+#include "string.h"
+
+#define DIAG_MSG_SIZE 1024+4
 
 typedef struct DiagMessage {
+    uint16_t id;
     uint16_t data_size;
     uint8_t data[1024]; // 1KB messages max (EGS52/3 is always max 256 bytes)
 };
@@ -13,15 +19,63 @@ typedef struct DiagMessage {
  * 
  */
 class AbstractEndpoint {
-    
+public:
+    AbstractEndpoint();
+    virtual void send_data(DiagMessage* msg); // Blocking operation
+    virtual bool read_data(DiagMessage* dest);
 };
 
 /**
  * @brief Endpoint for USB communication with ultimate-nag52 custom USB util
  * 
  */
-class UsbEndpoint: public AbstractEndpoint {
 
+#define UART_NUM 
+class UsbEndpoint: public AbstractEndpoint {
+    public:
+        UsbEndpoint() : AbstractEndpoint() {
+
+        }
+
+        void send_data(DiagMessage* msg) override {
+            // 2 chars per byte, 2 bytes for (CID), 2 bytes for '#' and '\n'
+            int size = (msg->data_size*2)+4;
+            char* buffer = (char*)malloc(size);
+            if (buffer == nullptr) {
+                ESP_LOGE("USBEndpoint", "Failed to allocate buffer for Tx DiagMessage!");
+                return;
+            }
+            buffer[0] = '#';
+            buffer[1] = (msg->id >> 8) & 0xFF;
+            buffer[2] = msg->id & 0xFF;
+            int len = 3;
+            for (uint16_t i = 0; i < msg->data_size; i++) {
+                sprintf(buffer+len, "%02X", msg->data[i]);
+            }
+            buffer[size-1] = '\n';
+            uart_write_bytes(0, buffer, size);
+            free(buffer);
+        }
+
+        bool read_data(DiagMessage* dest) override {
+            size_t length = 0;
+            uart_get_buffered_data_len(0, &length);
+            if (length != 0) {
+                int max_read = DIAG_MSG_SIZE-this->read_pos;
+                this->read_pos += uart_read_bytes(0, &this->read_buffer[this->read_pos], max_read, 2);
+                if (this->read_pos >= DIAG_MSG_SIZE) {
+                    dest->id = read_buffer[0] << 8 | read_buffer[1];
+                    dest->data_size = read_buffer[2] << 8 | read_buffer[3];
+                    memcpy(dest->data, &read_buffer[4], dest->data_size); // Only copy what we want rather than entire 1024 data
+                    this->read_pos = 0; // Reset
+                    return true;
+                }
+            }
+            return false;
+        }
+    private:
+        uint8_t read_buffer[DIAG_MSG_SIZE];
+        uint16_t read_pos;
 };
 
 /**
@@ -29,7 +83,7 @@ class UsbEndpoint: public AbstractEndpoint {
  * 
  */
 class CanEndpoint: public AbstractEndpoint {
-
+    
 };
 
 
---
+++ src/diag/kwp2000.h
@@ -0,0 +1,12 @@
+
+#ifndef _KWP_H__
+#define _KWP_H__
+
+#include <gearbox_config.h>
+
+#define KWP_ECU_TX_ID 0x07E9
+#define KWP_ECU_RX_ID 0x07E1
+
+
+
+#endif //_KWP_H__

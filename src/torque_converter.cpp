231067482dcf3cf7f72b734658decea79f5849b5
Ashcon Mohseninia <ashconm@outlook.com>
2022-05-15 13:19:41 +0100

    TCC: Modify increase PWM behaviour

--- src/torque_converter.cpp
+++ src/torque_converter.cpp
@@ -73,17 +73,15 @@ void TorqueConverter::update(GearboxGear curr_gear, PressureManager* pm, Abstrac
         min_allowed_slip *= multiplier;
     }
     int slip = sensors->tcc_slip_rpm;
-    if (is_shifting) {
+    if (is_shifting || mpc_offset != 0 || sensors->current_timestamp_ms-sensors->last_shift_time < 1500) {
         goto write_pwm;
     }
     if (slip > max_allowed_slip) {
+        int midpoint = (max_allowed_slip+min_allowed_slip*2)/3;
+        float diff = slip - midpoint;
         // Increase pressure
-        if (TCC_PREFILL+this->curr_tcc_pwm < 4500) {
-            this->curr_tcc_pwm += MAX(0.2, (sensors->pedal_pos/7)) * pm->get_tcc_temp_multiplier(sensors->atf_temp);
-        } else {
-            this->curr_tcc_pwm += MAX(0.1, (sensors->pedal_pos/15)) * pm->get_tcc_temp_multiplier(sensors->atf_temp);
-        }
-    } else if (sensors->tcc_slip_rpm < min_allowed_slip && this->curr_tcc_pwm >= 0 && sensors->pedal_pos > 5 && sensors->static_torque > 50) {
+        this->curr_tcc_pwm += MAX(0.1, (diff/15.0)) * pm->get_tcc_temp_multiplier(sensors->atf_temp);
+    } else if (sensors->tcc_slip_rpm < min_allowed_slip && this->curr_tcc_pwm >= 0 && sensors->static_torque > 80) {
         // Decrease pressure, but only if we have pedal input
         this->curr_tcc_pwm -= 0.5 * pm->get_tcc_temp_multiplier(sensors->atf_temp);
     }

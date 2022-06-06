9f650ef0598fb72eb2a27df9e31eb37250bc617d
Ashcon Mohseninia <ashconm@outlook.com>
2022-06-06 17:58:29 +0100

    PM: Add pressure in mbar rather than PWM

--- src/pressure_manager.cpp
+++ src/pressure_manager.cpp
@@ -1,38 +1,5 @@
 #include "pressure_manager.h"
-
-/*
-float find_temp_multiplier(int temp_c) {
-    int temp_raw = temp_c+50;
-    if (temp_raw < 0) { return pressure_temp_normalizer[0]; }
-    else if (temp_raw > 160) { return pressure_temp_normalizer[16]; }
-    int min = temp_raw/10;
-    int max = min+1;
-    float dy = pressure_temp_normalizer[max] - pressure_temp_normalizer[min];
-    float dx = (max-min)*10;
-    return (pressure_temp_normalizer[min] + ((dy/dx)) * (temp_raw-(min*10)));
-}
-*/
-
-float find_temp_ramp_speed_multiplier(int temp_c) {
-    int temp_raw = temp_c+50;
-    if (temp_raw < 0) { return ramp_speed_temp_normalizer[0]; }
-    else if (temp_raw > 160) { return ramp_speed_temp_normalizer[16]; }
-    int min = temp_raw/10;
-    int max = min+1;
-    float dy = ramp_speed_temp_normalizer[max] - ramp_speed_temp_normalizer[min];
-    float dx = (max-min)*10;
-    return (ramp_speed_temp_normalizer[min] + ((dy/dx)) * (temp_raw-(min*10)));
-}
-
-float find_rpm_pressure_multiplier(int engine_rpm) {
-    if (engine_rpm <= 0) { return rpm_normalizer[0]; }
-    else if (engine_rpm > 8000) { return rpm_normalizer[8]; }
-    int min = engine_rpm/1000;
-    int max = min+1;
-    float dy = rpm_normalizer[max] - rpm_normalizer[min];
-    float dx = (max-min)*1000;
-    return (rpm_normalizer[min] + ((dy/dx)) * (engine_rpm-(min*1000)));
-}
+#include <tcm_maths.h>
 
 inline uint16_t locate_pressure_map_value(const pressure_map map, int percent) {
     if (percent <= 0) { return map[0]; }
@@ -46,61 +13,61 @@ inline uint16_t locate_pressure_map_value(const pressure_map map, int percent) {
     }
 }
 
-uint16_t find_spc_pressure(const pressure_map map, SensorData* sensors, int max_rated_torque) {
-    // MPC reacts to Torque (Also sets pressure for SPC. Shift firmness can be increased)
-    int load = sensors->static_torque*100/max_rated_torque;
-    if (load < 0) { load *= -1; } // Pulling engine
-    //int load = (sensors->pedal_pos*100/250); // Try same as SPC
-    // For now forget about shift_firmness (TODO work on shift_firmness 1-10 to 1.1-0.9)
-    return locate_pressure_map_value(map, load);
-    //int load = (sensors->pedal_pos*100/250);
-    //return locate_pressure_map_value(map, load); //* find_temp_multiplier(sensors->atf_temp);
-}
-
-uint16_t find_mpc_pressure(const pressure_map map, SensorData* sensors, int max_rated_torque) {
-    // MPC reacts to Torque (Also sets pressure for SPC. Shift firmness can be increased)
-    int load = sensors->static_torque*100/max_rated_torque;
-    if (load < 0) { load *= -1; } // Pulling engine
-    //int load = (sensors->pedal_pos*100/250); // Try same as SPC
-    // For now forget about shift_firmness (TODO work on shift_firmness 1-10 to 1.1-0.9)
-    return locate_pressure_map_value(map, load);
-}
-
-#define COPY_MAP(name) \
-    memcpy(this->map_data.name, large_nag ? &name##_large : &name##_small, sizeof(pressure_map));
-
 PressureManager::PressureManager(SensorData* sensor_ptr) {
     this->sensor_data = sensor_ptr;
     this->adapt_map = new AdaptationMap();
-    this->map_data = PressureMgrData{};
-    bool large_nag = VEHICLE_CONFIG.is_large_nag;
-    COPY_MAP(mpc_1_2)
-    COPY_MAP(spc_1_2)
-    COPY_MAP(mpc_2_3)
-    COPY_MAP(spc_2_3)
-    COPY_MAP(mpc_3_4)
-    COPY_MAP(spc_3_4)
-    COPY_MAP(mpc_4_5)
-    COPY_MAP(spc_4_5)
-    COPY_MAP(mpc_5_4)
-    COPY_MAP(spc_5_4)
-    COPY_MAP(mpc_4_3)
-    COPY_MAP(spc_4_3)
-    COPY_MAP(mpc_3_2)
-    COPY_MAP(spc_3_2)
-    COPY_MAP(mpc_2_1)
-    COPY_MAP(spc_2_1)
-    memcpy(this->map_data.ramp_speed_multiplier, rpm_normalizer, sizeof(rpm_modifier_map));
-    memcpy(this->map_data.working_mpc_p, working_norm_pressure_p, sizeof(pressure_map));
-    memcpy(this->map_data.working_mpc_r, working_norm_pressure_r, sizeof(pressure_map));
-    memcpy(this->map_data.working_mpc_1, working_norm_pressure_1, sizeof(pressure_map));
-    memcpy(this->map_data.working_mpc_2, working_norm_pressure_2, sizeof(pressure_map));
-    memcpy(this->map_data.working_mpc_3, working_norm_pressure_3, sizeof(pressure_map));
-    memcpy(this->map_data.working_mpc_4, working_norm_pressure_4, sizeof(pressure_map));
-    memcpy(this->map_data.working_mpc_5, working_norm_pressure_5, sizeof(pressure_map));
     memset(&this->tcc, 0, sizeof(PSolenoidData));
     memset(&this->spc, 0, sizeof(PSolenoidData));
     memset(&this->mpc, 0, sizeof(PSolenoidData));
+
+    /** Pressure PWM map **/
+
+    int16_t pwm_x_headers[7] = {50, 600, 1000, 2350, 5600, 6600, 7700};
+    int16_t pwm_y_headers[4] = {-25, 20, 60, 150}; 
+
+    pressure_pwm_map = new TcmMap(7, 4, pwm_x_headers, pwm_y_headers);
+
+    if (!this->pressure_pwm_map->allocate_ok()) {
+        this->pressure_pwm_map = nullptr;
+        ESP_LOGE("PM", "Allocation of pressure pwm map failed!");
+        return;
+    }
+    // Allocation OK, add the data to the map
+
+    // Values are PWM % (0-1000) for SPC and MPC solenoid
+    // This table takes into account resistance change based on ATF temp
+    int16_t pwm_map[7*4] = {
+    /*              50   600  1000  2350  5600  6600  7700 <- mBar */
+    /* -25C */     396,  391,  344,  252,  162,  126,   72,
+    /*  20C */     472,  406,  364,  296,  182,  140,    0,
+    /*  60C */     508,  424,  396,  330,  203,  146,    0,
+    /* 150C */     648,  529,  495,  412,  246,  173,    0
+    };
+    pressure_pwm_map->add_data(pwm_map, 7*4);
+
+
+    /** Pressure Hold 2 time map **/
+    int16_t hold2_x_headers[5] = {1, 2, 3, 4, 5};
+    int16_t hold2_y_headers[4] = {-20, 5, 25, 60}; 
+
+    hold2_time_map = new TcmMap(5, 4, hold2_x_headers, hold2_y_headers);
+
+    if (!this->hold2_time_map->allocate_ok()) {
+        this->hold2_time_map = nullptr;
+        ESP_LOGE("PM", "Allocation of pressure pwm map failed!");
+        return;
+    }
+    // Allocation OK, add the data to the map
+
+    // Values are in millsecond for SPC to hold at Hold2 phase
+    int16_t hold2_map[5*4] = {
+    /* Curr gear    1      2     3     4     5 */
+    /* -20C */     600, 1620,  860,  600,  820,
+    /*   5C */     300,  600,  440,  380,  400,
+    /*  25C */     180,  240,  160,  220,  180,
+    /*  60C */     160,  140,  140,  180,  120
+    };
+    hold2_time_map->add_data(hold2_map, 5*4);
 }
 
 uint16_t PressureManager::find_working_mpc_pressure(GearboxGear curr_g, SensorData* sensors, int max_rated_torque) {
@@ -109,26 +76,26 @@ uint16_t PressureManager::find_working_mpc_pressure(GearboxGear curr_g, SensorDa
         torque *= -1;
     }
 
-    int16_t* targ_map = map_data.working_mpc_p;
+    const int16_t* targ_map = working_norm_pressure_p;
     switch(curr_g) {
         case GearboxGear::First:
-            targ_map = map_data.working_mpc_1;
+            targ_map = working_norm_pressure_1;
             break;
         case GearboxGear::Second:
-            targ_map = map_data.working_mpc_2;
+            targ_map = working_norm_pressure_2;
             break;
         case GearboxGear::Third:
-            targ_map = map_data.working_mpc_3;
+            targ_map = working_norm_pressure_3;
             break;
         case GearboxGear::Fourth:
-            targ_map = map_data.working_mpc_4;
+            targ_map = working_norm_pressure_4;
             break;
         case GearboxGear::Fifth:
-            targ_map = map_data.working_mpc_5;
+            targ_map = working_norm_pressure_5;
             break;
         case GearboxGear::Reverse_First:
         case GearboxGear::Reverse_Second:
-            targ_map = map_data.working_mpc_r;
+            targ_map = working_norm_pressure_r;
             break;
         case GearboxGear::Park:
         case GearboxGear::Neutral:
@@ -143,75 +110,83 @@ uint16_t PressureManager::find_working_mpc_pressure(GearboxGear curr_g, SensorDa
 }
 
 float PressureManager::get_tcc_temp_multiplier(int atf_temp) {
-    return 1.0/find_temp_ramp_speed_multiplier(atf_temp);
+    return 1.0;
 }
 
 ShiftData PressureManager::get_shift_data(SensorData* sensors, ProfileGearChange shift_request, ShiftCharacteristics chars, int max_rated_torque) {
     ShiftData sd; 
     switch (shift_request) {
         case ProfileGearChange::ONE_TWO:
-            sd.initial_spc_pwm = find_spc_pressure(map_data.spc_1_2, this->sensor_data, max_rated_torque);
-            sd.initial_mpc_pwm = find_mpc_pressure(map_data.mpc_1_2, this->sensor_data, max_rated_torque);
             sd.targ_g = 2; sd.curr_g = 1;
             sd.shift_solenoid = sol_y3;
-            sd.torque_cut_multiplier = 0.8;
             break;
         case ProfileGearChange::TWO_THREE:
-            sd.initial_spc_pwm = find_spc_pressure(map_data.spc_2_3, this->sensor_data, max_rated_torque);
-            sd.initial_mpc_pwm = find_mpc_pressure(map_data.mpc_2_3, this->sensor_data, max_rated_torque);
             sd.targ_g = 3; sd.curr_g = 2;
             sd.shift_solenoid = sol_y5;
-            sd.torque_cut_multiplier = 0.8;
             break;
         case ProfileGearChange::THREE_FOUR:
-            sd.initial_spc_pwm = find_spc_pressure(map_data.spc_3_4, this->sensor_data, max_rated_torque);
-            sd.initial_mpc_pwm = find_mpc_pressure(map_data.mpc_3_4, this->sensor_data, max_rated_torque);
             sd.targ_g = 4; sd.curr_g = 3;
             sd.shift_solenoid = sol_y4;
-            sd.torque_cut_multiplier = 0.8;
             break;
         case ProfileGearChange::FOUR_FIVE:
-            sd.initial_spc_pwm = find_spc_pressure(map_data.spc_4_5, this->sensor_data, max_rated_torque);
-            sd.initial_mpc_pwm = find_mpc_pressure(map_data.mpc_4_5, this->sensor_data, max_rated_torque);
             sd.targ_g = 5; sd.curr_g = 4;
             sd.shift_solenoid = sol_y3;
-            sd.torque_cut_multiplier = 0.8;
             break;
         case ProfileGearChange::FIVE_FOUR:
-            sd.initial_spc_pwm = find_spc_pressure(map_data.spc_5_4, this->sensor_data, max_rated_torque);
-            sd.initial_mpc_pwm = find_mpc_pressure(map_data.mpc_5_4, this->sensor_data, max_rated_torque);
             sd.targ_g = 4; sd.curr_g = 5;
             sd.shift_solenoid = sol_y3;
-            sd.torque_cut_multiplier = 0.9;
             break;
         case ProfileGearChange::FOUR_THREE:
-            sd.initial_spc_pwm = find_spc_pressure(map_data.spc_4_3, this->sensor_data, max_rated_torque);
-            sd.initial_mpc_pwm = find_mpc_pressure(map_data.mpc_4_3, this->sensor_data, max_rated_torque);
             sd.targ_g = 3; sd.curr_g = 4;
             sd.shift_solenoid = sol_y4;
-            sd.torque_cut_multiplier = 0.9;
             break;
         case ProfileGearChange::THREE_TWO:
-            sd.initial_spc_pwm = find_spc_pressure(map_data.spc_3_2, this->sensor_data, max_rated_torque);
-            sd.initial_mpc_pwm = find_mpc_pressure(map_data.mpc_3_2, this->sensor_data, max_rated_torque);
             sd.targ_g = 2; sd.curr_g = 3;
             sd.shift_solenoid = sol_y5;
-            sd.torque_cut_multiplier = 0.9;
             break;
         case ProfileGearChange::TWO_ONE:
-            sd.initial_spc_pwm = find_spc_pressure(map_data.spc_2_1, this->sensor_data, max_rated_torque);
-            sd.initial_mpc_pwm = find_mpc_pressure(map_data.mpc_2_1, this->sensor_data, max_rated_torque);
             sd.targ_g = 1; sd.curr_g = 2;
             sd.shift_solenoid = sol_y3;
-            sd.torque_cut_multiplier = 0.9;
             break;
     }
-    sd.spc_dec_speed = chars.shift_speed;// * find_temp_ramp_speed_multiplier(sensors->atf_temp));
-    //if (this->adapt_map != nullptr) {
-    //    sd.initial_spc_pwm += adapt_map->get_adaptation_offset(sensors, shift_request);
-    //}
-    sd.temp_multi =  find_temp_ramp_speed_multiplier(sensors->atf_temp);
+
+
+    sd.hold1_data.ramp_time = 100;
+    sd.hold1_data.hold_time = 100;
+    sd.hold1_data.pwm_percent = this->get_p_solenoid_pwm_percent(50);
+
+    sd.hold2_data.ramp_time = 0; // Hold 2 has no ramp time
+    sd.hold2_data.hold_time = hold2_time_map->get_value(sd.targ_g, sensors->atf_temp);
+    sd.hold2_data.pwm_percent = this->get_p_solenoid_pwm_percent(200);
+
+    sd.hold3_data.ramp_time = 60;
+    sd.hold3_data.hold_time = 100;
+    sd.hold3_data.pwm_percent = this->get_p_solenoid_pwm_percent(1300);
+
+    sd.torque_data.ramp_time = 400;
+    sd.torque_data.hold_time = 400;
+    sd.torque_data.pwm_percent = this->get_p_solenoid_pwm_percent(2000);
+
+    sd.overlap_data.ramp_time = 200;
+    if (shift_request == ProfileGearChange::ONE_TWO) {
+        sd.overlap_data.hold_time = 20;
+    } else {
+        sd.overlap_data.hold_time = 0;
+    }
+    sd.overlap_data.pwm_percent = this->get_p_solenoid_pwm_percent(3000);
+
+    sd.max_pressure_data.ramp_time = 100;
+    sd.max_pressure_data.hold_time = 500;
+    sd.max_pressure_data.pwm_percent = this->get_p_solenoid_pwm_percent(6000);
+
     return sd;
+}
+
+uint16_t PressureManager::get_p_solenoid_pwm_percent(uint16_t request_mbar) {
+    if (this->pressure_pwm_map == nullptr) {
+        return 100; // 10% (Failsafe)
+    }
+    return this->pressure_pwm_map->get_value(request_mbar, this->sensor_data->atf_temp);
 }
 
 
@@ -263,13 +238,6 @@ void PressureManager::set_target_tcc_percent(uint16_t targ, int speed) {
     }
 }
 
-void PressureManager::set_spc_compensation_factor(float amount) {
-    if (amount < 0) {
-        amount = 0;
-    }
-    this->spc_compensation_amount = amount;
-}
-
 void PressureManager::update(GearboxGear curr_gear, GearboxGear targ_gear) {
 
     if (this->spc.write_pwm) {
@@ -311,12 +279,8 @@ void PressureManager::update(GearboxGear curr_gear, GearboxGear targ_gear) {
             }
         }
     }
-    float spc_compensation = ((float)(this->spc.curr_pwm_percent/4.0)) * this->spc_compensation_amount;
-    if (spc_compensation > this->mpc.curr_pwm_percent) {
-        spc_compensation = this->mpc.curr_pwm_percent;
-    }
     // Always write MPC as it regulates everyone else
     sol_spc->write_pwm_percent_with_voltage(this->spc.curr_pwm_percent, this->sensor_data->voltage);
-    sol_mpc->write_pwm_percent_with_voltage(this->mpc.curr_pwm_percent-spc_compensation, this->sensor_data->voltage);
+    sol_mpc->write_pwm_percent_with_voltage(this->mpc.curr_pwm_percent, this->sensor_data->voltage);
     sol_tcc->write_pwm_percent_with_voltage(this->tcc.curr_pwm_percent, this->sensor_data->voltage);
 }

baab2bb9c134f5bde615e44898d967195b0afa3a
Ashcon Mohseninia <ashconm@outlook.com>
2022-06-07 21:07:15 +0100

    TEST: Tweak map test

--- test/main.cpp
+++ test/main.cpp
@@ -5,14 +5,14 @@
 
 void test_map(void) {
 
-    int16_t x_headers[7] = {5, 60, 100, 235, 560, 660, 770};
-    int16_t y_headers[4] = {-25, 20, 60, 150};
+    int16_t x_headers[7] = {1, 2, 4, 8, 16, 32, 64};
+    int16_t y_headers[4] = {-5, 0, 5, 10};
 
     int16_t data[7*4] = {
-        1100, 1085, 954, 700, 450, 350, 200,
-        1077,  925, 830, 675, 415, 320,   0,
-        1000,  835, 780, 650, 400, 288,   0,
-         975,  795, 745, 625, 370, 260,   0
+        0  , 100,  200,  400,  800, 1600, 3200,
+        100, 200,  400,  800, 1600, 3200, 6400,
+        200, 400,  800, 1600, 3200, 6400, 6400,
+        400, 800, 1600, 3200, 6400, 6400, 6400
     };
 
     TcmMap test_map = TcmMap(7, 4, x_headers, y_headers);
@@ -26,11 +26,14 @@ void test_map(void) {
     for (int i = 0; i < 7; i++) {
         UNITY_TEST_ASSERT_EQUAL_INT16(ptr_src[i], ptr[i], TEST_LINE_NUM, "Data mismatch!");
     }
-    UNITY_TEST_ASSERT_EQUAL_FLOAT(925.0, test_map.get_value(60, 20), TEST_LINE_NUM, "Data mismatch point lookup"); // 60mbar, 20C - Both X and Y are on points in table
+    UNITY_TEST_ASSERT_EQUAL_FLOAT(200, test_map.get_value(2, 0), TEST_LINE_NUM, "Data mismatch point lookup");
 
-    UNITY_TEST_ASSERT_EQUAL_FLOAT(877.5, test_map.get_value(80, 20), TEST_LINE_NUM, "Data mismatch X interpolate!"); // 80mbar, 20C - X axis linear interpolate
+    UNITY_TEST_ASSERT_EQUAL_FLOAT(300, test_map.get_value(3, 0), TEST_LINE_NUM, "Data mismatch X 50/50 interpolate!");
 
-    UNITY_TEST_ASSERT_EQUAL_FLOAT(880, test_map.get_value(60, 40), TEST_LINE_NUM, "Data mismatch Y interpolate!");// 60mbar, 40C - Y axis linear interpolation
+    UNITY_TEST_ASSERT_EQUAL_FLOAT(150, test_map.get_value(2, -2.5), TEST_LINE_NUM, "Data mismatch Y 50/50 interpolate!");
+
+    UNITY_TEST_ASSERT_EQUAL_FLOAT(500, test_map.get_value(5, 0), TEST_LINE_NUM, "Data mismatch X 25/75 interpolate!");
+    UNITY_TEST_ASSERT_EQUAL_FLOAT(500, test_map.get_value(2, 6.25), TEST_LINE_NUM, "Data mismatch Y 25/75 interpolate!");
 
 }
 

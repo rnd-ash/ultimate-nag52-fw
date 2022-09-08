dd2d04dccb7b8bc531b669bb91a6ccf345f67e00
Ashcon Mohseninia <ashconm@outlook.com>
2022-09-08 21:34:53 +0100

    Add map R/W framework

---
+++ src/diag/map_editor.cpp
@@ -0,0 +1,13 @@
+#include "map_editor.h"
+
+
+
+uint8_t MapEditor::read_map_data(uint8_t map_id, uint16_t *dest_size, int16_t* buffer) {
+    
+}
+    
+uint8_t MapEditor::write_map_data(uint8_t map_id, uint16_t dest_size, int16_t* buffer) {
+    
+}
+
+

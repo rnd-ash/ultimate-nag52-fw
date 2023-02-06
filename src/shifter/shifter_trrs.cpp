3d4811eadce3c9f8c2dfe0f67c2573343d314098
Christian C. Völzke <christian@voelzke.biz>
2023-02-06 21:19:43 +0100

    Setting of starter pin.

--- src/shifter/shifter_trrs.cpp
+++ src/shifter/shifter_trrs.cpp
@@ -62,13 +62,14 @@ ShifterPosition ShifterTrrs::get_shifter_position(const uint64_t now, const uint
 			TRRS_C = (tmp & (uint8_t)BIT(4u)) != 0;
 			TRRS_D = (tmp & (uint8_t)BIT(3u)) != 0;
 		}
-
+		*_start_enable = false;
 		if (!TRRS_A && !TRRS_B && !TRRS_C && !TRRS_D)
 		{ // Intermediate position, now work out which one
 			switch (this->last_valid_position)
 			{
 			case ShifterPosition::P:
 				ret = ShifterPosition::P_R;
+				*_start_enable = true;
 				break;
 			case ShifterPosition::R:
 				ret = ShifterPosition::R_N;
@@ -78,6 +79,7 @@ ShifterPosition ShifterTrrs::get_shifter_position(const uint64_t now, const uint
 				break;
 			case ShifterPosition::N:
 				ret = ShifterPosition::N_D;
+				*_start_enable = true;
 				break;
 			default:
 				ret = ShifterPosition::SignalNotAvailable;
@@ -87,7 +89,7 @@ ShifterPosition ShifterTrrs::get_shifter_position(const uint64_t now, const uint
 		else
 		{
 			// Check truth table
-			for (uint8_t i = 0; i < 8; i++)
+			for (uint8_t i = 0u; i < 8u; i++)
 			{
 				TRRSPos pos = TRRS_SHIFTER_TABLE[i];
 				if (pos.a == TRRS_A && pos.b == TRRS_B && pos.c == TRRS_C && pos.d == TRRS_D)
@@ -107,11 +109,11 @@ void ShifterTrrs::update_shifter_position(const uint64_t now)
             // Query I2C IO Expander
             uint8_t req[2] = {0,0};
             esp_err_t e = i2c_master_write_read_device(I2C_NUM_0, IO_ADDR, req, 1, i2c_rx_bytes, 2, 5);
-            if (ESP_OK != e) {
+            if (ESP_OK == e) {
+				last_i2c_query_time = now;
+            } else {
                 // Error, SNV
                 ESP_LOGE("LS", "Could not query I2C: %s", esp_err_to_name(e));
-            } else {
-				last_i2c_query_time = now;
             }
             
             // Set RP and Start pins on IO expander to be outputs
@@ -119,7 +121,7 @@ void ShifterTrrs::update_shifter_position(const uint64_t now)
             // IO 2-7 - INPUT
             uint8_t write_buffer[2] = {0x07,0xFF}; // Set IO (0x06 + port 1 (0x01))
             if (_start_enable) {
-                write_buffer[1] = write_buffer[1] & ~(BIT(1));
+                write_buffer[1] &= ~(BIT(1));
             }
 
             i2c_master_write_to_device(I2C_NUM_0, IO_ADDR, write_buffer, 2, 50);

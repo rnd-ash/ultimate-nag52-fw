3e1f66c5f33fe435510e075d12528b6d05907840
Ashcon Mohseninia <ashconm@outlook.com>
2023-04-06 08:42:41 +0100

    Make default map data const

--- src/maps.cpp
+++ src/maps.cpp
@@ -1,6 +1,6 @@
 #include "maps.h"
 
-int16_t S_DIESEL_UPSHIFT_MAP[SHIFT_MAP_SIZE] = {
+const int16_t S_DIESEL_UPSHIFT_MAP[SHIFT_MAP_SIZE] = {
     /*                        Pedal position                                   */
     /*0%   10%   20%   30%   40%   50%   60%   70%   80%   90%  100%           */
     1400, 1550, 1800, 2000, 2200, 2450, 2600, 2850, 3200, 4000, 4500,/* 1 -> 2 */
@@ -10,7 +10,7 @@ int16_t S_DIESEL_UPSHIFT_MAP[SHIFT_MAP_SIZE] = {
 };
 
 
-int16_t S_DIESEL_DOWNSHIFT_MAP[SHIFT_MAP_SIZE] = {
+const int16_t S_DIESEL_DOWNSHIFT_MAP[SHIFT_MAP_SIZE] = {
     /*                        Pedal position                                   */
     /*0%   10%   20%   30%   40%   50%   60%   70%   80%   90%  100%           */
        100, 500, 850, 1050, 1200, 1400, 1500, 1700, 1800, 1900, 2000,/* 2 -> 1 */
@@ -19,7 +19,7 @@ int16_t S_DIESEL_DOWNSHIFT_MAP[SHIFT_MAP_SIZE] = {
      900, 1000, 1100, 1200, 1350, 1450, 1500, 1750, 1900, 2200, 2500 /* 5 -> 4 */
     };
 
-int16_t S_PETROL_UPSHIFT_MAP[SHIFT_MAP_SIZE] = {
+const int16_t S_PETROL_UPSHIFT_MAP[SHIFT_MAP_SIZE] = {
     /*                        Pedal position                                   */
     /*0%   10%   20%   30%   40%   50%   60%   70%   80%   90%  100%           */
     1500, 1600, 1700, 1800, 1900, 2200, 2500, 3000, 3500, 4500, 6000,/* 1 -> 2 */
@@ -29,7 +29,7 @@ int16_t S_PETROL_UPSHIFT_MAP[SHIFT_MAP_SIZE] = {
 };
 
 
-int16_t S_PETROL_DOWNSHIFT_MAP[SHIFT_MAP_SIZE] = {
+const int16_t S_PETROL_DOWNSHIFT_MAP[SHIFT_MAP_SIZE] = {
     /*                        Pedal position                                   */
     /*0%   10%   20%   30%   40%   50%   60%   70%   80%   90%  100%           */
       75,  150,  300,  600,  900, 1200, 1400, 1600, 1800, 2000, 2200,/* 2 -> 1 */
@@ -44,7 +44,7 @@ int16_t S_PETROL_DOWNSHIFT_MAP[SHIFT_MAP_SIZE] = {
 ---------------------------------------------------------------------------------
 */
 
-int16_t C_DIESEL_UPSHIFT_MAP[SHIFT_MAP_SIZE] = {
+const int16_t C_DIESEL_UPSHIFT_MAP[SHIFT_MAP_SIZE] = {
     /*                        Pedal position                                   */
     /*0%   10%   20%   30%   40%   50%   60%   70%   80%   90%  100%           */
     1500, 1750, 1900, 2100, 2200, 2450, 2600, 3000, 3500, 4000, 4500,/* 1 -> 2 */
@@ -53,7 +53,7 @@ int16_t C_DIESEL_UPSHIFT_MAP[SHIFT_MAP_SIZE] = {
     1450, 1500, 1600, 1650, 1900, 2200, 2500, 3000, 3700, 4200, 4500 /* 4 -> 5 */
 };
 
-int16_t C_DIESEL_DOWNSHIFT_MAP[SHIFT_MAP_SIZE] = {
+const int16_t C_DIESEL_DOWNSHIFT_MAP[SHIFT_MAP_SIZE] = {
     /*                        Pedal position                                   */
     /*0%   10%   20%   30%   40%   50%   60%   70%   80%   90%  100%           */
        0,    0,    0,    0,  300,  400,  500,  600,  700,  800,  900,/* 2 -> 1 */
@@ -62,7 +62,7 @@ int16_t C_DIESEL_DOWNSHIFT_MAP[SHIFT_MAP_SIZE] = {
      900,  900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800 /* 5 -> 4 */
 };
 
-int16_t C_PETROL_UPSHIFT_MAP[SHIFT_MAP_SIZE] = {
+const int16_t C_PETROL_UPSHIFT_MAP[SHIFT_MAP_SIZE] = {
     /*                        Pedal position                                   */
     /*0%   10%   20%   30%   40%   50%   60%   70%   80%   90%  100%           */
     1500, 1600, 1700, 1800, 1900, 2200, 2500, 3000, 3500, 4500, 6000,/* 1 -> 2 */
@@ -71,7 +71,7 @@ int16_t C_PETROL_UPSHIFT_MAP[SHIFT_MAP_SIZE] = {
     1500, 1550, 1600, 1700, 1900, 2000, 2500, 3000, 3500, 4500, 6000 /* 4 -> 5 */
 };
 
-int16_t C_PETROL_DOWNSHIFT_MAP[SHIFT_MAP_SIZE] = {
+const int16_t C_PETROL_DOWNSHIFT_MAP[SHIFT_MAP_SIZE] = {
     /*                        Pedal position                                   */
     /*0%   10%   20%   30%   40%   50%   60%   70%   80%   90%  100%           */
        0,    0,    0,    0,  300,  400,  500,  600,  700,  800,  900,/* 2 -> 1 */
@@ -86,7 +86,7 @@ int16_t C_PETROL_DOWNSHIFT_MAP[SHIFT_MAP_SIZE] = {
 ---------------------------------------------------------------------------------
 */
 
-int16_t A_DIESEL_UPSHIFT_MAP[SHIFT_MAP_SIZE] = {
+const int16_t A_DIESEL_UPSHIFT_MAP[SHIFT_MAP_SIZE] = {
     /*                        Pedal position                                   */
     /*0%   10%   20%   30%   40%   50%   60%   70%   80%   90%  100%           */
     1400, 1550, 1700, 2050, 2300, 2600, 2900, 3150, 3500, 4000, 4500,/* 1 -> 2 */
@@ -95,7 +95,7 @@ int16_t A_DIESEL_UPSHIFT_MAP[SHIFT_MAP_SIZE] = {
     1450, 1550, 1650, 1850, 2050, 2250, 2500, 2700, 3100, 3700, 4500 /* 4 -> 5 */
 };
 
-int16_t A_DIESEL_DOWNSHIFT_MAP[SHIFT_MAP_SIZE] = {
+const int16_t A_DIESEL_DOWNSHIFT_MAP[SHIFT_MAP_SIZE] = {
     /*                        Pedal position                                   */
     /*0%   10%   20%   30%   40%   50%   60%   70%   80%   90%  100%           */
        100, 500, 900, 1100, 1200, 1400, 1600, 1700, 1800, 2000, 2300,/* 2 -> 1 */
@@ -104,7 +104,7 @@ int16_t A_DIESEL_DOWNSHIFT_MAP[SHIFT_MAP_SIZE] = {
       900, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1900, 2200 /* 5 -> 4 */
 };
 
-int16_t A_PETROL_UPSHIFT_MAP[SHIFT_MAP_SIZE] = {
+const int16_t A_PETROL_UPSHIFT_MAP[SHIFT_MAP_SIZE] = {
     /*                        Pedal position                                   */
     /*0%   10%   20%   30%   40%   50%   60%   70%   80%   90%  100%           */
     1500, 1600, 1700, 1800, 1900, 2200, 2500, 3000, 3500, 4500, 6000,/* 1 -> 2 */
@@ -113,7 +113,7 @@ int16_t A_PETROL_UPSHIFT_MAP[SHIFT_MAP_SIZE] = {
     1500, 1550, 1600, 1700, 1900, 2000, 2500, 3000, 3500, 4500, 6000 /* 4 -> 5 */
 };
 
-int16_t A_PETROL_DOWNSHIFT_MAP[SHIFT_MAP_SIZE] = {
+const int16_t A_PETROL_DOWNSHIFT_MAP[SHIFT_MAP_SIZE] = {
     /*                        Pedal position                                   */
     /*0%   10%   20%   30%   40%   50%   60%   70%   80%   90%  100%           */
       75,  150,  300,  600,  900, 1200, 1400, 1600, 1800, 2000, 2200,/* 2 -> 1 */
@@ -136,7 +136,7 @@ int16_t A_PETROL_DOWNSHIFT_MAP[SHIFT_MAP_SIZE] = {
                     MANUAL MAPS ARE NOT EDITABLE IN THE CONFIG APP!
 ---------------------------------------------------------------------------------
 */
-int16_t M_DIESEL_UPSHIFT_MAP[SHIFT_MAP_SIZE] = {
+const int16_t M_DIESEL_UPSHIFT_MAP[SHIFT_MAP_SIZE] = {
     /*                        Pedal position                                   */
     /*0%   10%   20%   30%   40%   50%   60%   70%   80%   90%  100%           */
     9999, 9999, 9999, 9999, 9999, 9999, 9999, 9999, 9999, 9999, 9999,/* 1 -> 2 */
@@ -145,7 +145,7 @@ int16_t M_DIESEL_UPSHIFT_MAP[SHIFT_MAP_SIZE] = {
     9999, 9999, 9999, 9999, 9999, 9999, 9999, 9999, 9999, 9999, 9999 /* 4 -> 5 */
 };
 
-int16_t M_DIESEL_DOWNSHIFT_MAP[SHIFT_MAP_SIZE] = {
+const int16_t M_DIESEL_DOWNSHIFT_MAP[SHIFT_MAP_SIZE] = {
     /*                        Pedal position                                   */
     /*0%   10%   20%   30%   40%   50%   60%   70%   80%   90%  100%           */
      300,  350,  400,  450,  500,  550,  600,  650,  700,  750,  800,/* 2 -> 1 */
@@ -154,7 +154,7 @@ int16_t M_DIESEL_DOWNSHIFT_MAP[SHIFT_MAP_SIZE] = {
      600,  650,  700,  750,  800,  850,  900,  950, 1000, 1050, 1100 /* 5 -> 4 */
 };
 
-int16_t M_PETROL_UPSHIFT_MAP[SHIFT_MAP_SIZE] = {
+const int16_t M_PETROL_UPSHIFT_MAP[SHIFT_MAP_SIZE] = {
     /*                        Pedal position                                   */
     /*0%   10%   20%   30%   40%   50%   60%   70%   80%   90%  100%           */
     9999, 9999, 9999, 9999, 9999, 9999, 9999, 9999, 9999, 9999, 9999,/* 1 -> 2 */
@@ -163,7 +163,7 @@ int16_t M_PETROL_UPSHIFT_MAP[SHIFT_MAP_SIZE] = {
     9999, 9999, 9999, 9999, 9999, 9999, 9999, 9999, 9999, 9999, 9999 /* 4 -> 5 */
 };
 
-int16_t M_PETROL_DOWNSHIFT_MAP[SHIFT_MAP_SIZE] = {
+const int16_t M_PETROL_DOWNSHIFT_MAP[SHIFT_MAP_SIZE] = {
     /*                        Pedal position                                   */
     /*0%   10%   20%   30%   40%   50%   60%   70%   80%   90%  100%           */
       75,  150,  300,  450,  500,  550,  600,  650,  700,  750,  800,/* 2 -> 1 */
@@ -174,7 +174,7 @@ int16_t M_PETROL_DOWNSHIFT_MAP[SHIFT_MAP_SIZE] = {
 
 
 
-int16_t NAG_WORKING_MAP[WORKING_PRESSURE_MAP_SIZE] = {
+const int16_t NAG_WORKING_MAP[WORKING_PRESSURE_MAP_SIZE] = {
    /* Input torque (% of rated) */
    /*0%,  10%   20%   30%   40%   50%   60%    70%   80%   90%  100%  110%  120%  130%  140%  150%+ */
     500,  600,  700,  800,  900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000, // P/N
@@ -187,7 +187,7 @@ int16_t NAG_WORKING_MAP[WORKING_PRESSURE_MAP_SIZE] = {
     750,  900, 1150, 1400, 1700, 2000, 2300, 2600, 2900, 3200, 3500, 3900, 4200, 4500, 4800, 5100  // 5
 };
 
-int16_t SMALL_NAG_FILL_TIME_MAP[FILL_TIME_MAP_SIZE] = {
+const int16_t SMALL_NAG_FILL_TIME_MAP[FILL_TIME_MAP_SIZE] = {
     /* ATF TEMP             */
     /*  -20C,  5C, 25C, 60C */
          600, 300, 180, 160, // K1 clutch
@@ -197,7 +197,7 @@ int16_t SMALL_NAG_FILL_TIME_MAP[FILL_TIME_MAP_SIZE] = {
          820, 400, 180, 120  // B2 brake
 };
 
-int16_t LARGE_NAG_FILL_TIME_MAP[FILL_TIME_MAP_SIZE] = {
+const int16_t LARGE_NAG_FILL_TIME_MAP[FILL_TIME_MAP_SIZE] = {
     /* ATF TEMP             */
     /*  -20C,  5C, 25C, 60C */
          600, 300, 180, 160, // K1 clutch
@@ -207,7 +207,7 @@ int16_t LARGE_NAG_FILL_TIME_MAP[FILL_TIME_MAP_SIZE] = {
          820, 400, 180, 120  // B2 brake
 };
 
-int16_t TCC_PWM_MAP[TCC_PWM_MAP_SIZE] = { // values are in /4096
+const int16_t TCC_PWM_MAP[TCC_PWM_MAP_SIZE] = { // values are in /4096
  /*         TCC PRESSURE (mBar)              */   
  /* 0   2000  4000  5000  7500  10000  15000 */
     0,   480,  960, 1280, 1920,  2560,  4096, // 0C
@@ -217,7 +217,7 @@ int16_t TCC_PWM_MAP[TCC_PWM_MAP_SIZE] = { // values are in /4096
     0,   640, 1120, 1280, 1920,  2560,  4096, // 120C
 };
 
-int16_t BROWN_PCS_CURRENT_MAP[PCS_CURRENT_MAP_SIZE] = { // values are in mA
+const int16_t BROWN_PCS_CURRENT_MAP[PCS_CURRENT_MAP_SIZE] = { // values are in mA
     /*      mBar                                    */
     /* 0    50    600  1000  2350  5600  6600  7700 */
     1300, 1100, 1085,  954,  700,  450,  350, 200, // -25C
@@ -226,7 +226,7 @@ int16_t BROWN_PCS_CURRENT_MAP[PCS_CURRENT_MAP_SIZE] = { // values are in mA
     1175,  975,  795,  745,  625,  370,  260,   0  // 150C
 };
 
-int16_t BLUE_PCS_CURRENT_MAP[PCS_CURRENT_MAP_SIZE] = {
+const int16_t BLUE_PCS_CURRENT_MAP[PCS_CURRENT_MAP_SIZE] = {
     /*               0    50    600  1000  2350  5600  6600  7700 <- mBar */
     /* -25C */     1300, 1100, 1085,  954,  700,  450,  350, 200,
     /*  20C */     1277, 1077,  925,  830,  675,  415,  320,   0,
@@ -234,19 +234,19 @@ int16_t BLUE_PCS_CURRENT_MAP[PCS_CURRENT_MAP_SIZE] = {
     /* 150C */     1175,  975,  795,  745,  625,  370,  260,   0
 };
 
-int16_t SMALL_NAG_FILL_PRESSURE_MAP[FILL_PRESSURE_MAP_SIZE] = {
+const int16_t SMALL_NAG_FILL_PRESSURE_MAP[FILL_PRESSURE_MAP_SIZE] = {
     /* Clutch                    */
    /* K1    K2    K3    B1    B2 */
     1200, 1400, 1300, 1200, 1400
 };
 
-int16_t LARGE_NAG_FILL_PRESSURE_MAP[FILL_PRESSURE_MAP_SIZE] = {
+const int16_t LARGE_NAG_FILL_PRESSURE_MAP[FILL_PRESSURE_MAP_SIZE] = {
     /* Clutch                    */
    /* K1    K2    K3    B1    B2 */
     1200, 1400, 1300, 1200, 1400
 };
 
-int16_t FILL_MPC_ADDER_MAP[FILL_PRESSURE_ADDER_MAP_SIZE] = {
+const int16_t FILL_MPC_ADDER_MAP[FILL_PRESSURE_ADDER_MAP_SIZE] = {
    /* Engine torque (0-100%) of 330Nm or 580Nm */
    /*0%,  10%   20%   30%   40%   50%   60%    70%   80%   90%  100%  */ // NOTE this map does not exceed 100%. Torque is restricted instead
      100,  200,  300,  400,  500,  600,  700,  800,  900, 1000, 1100, // K1
@@ -257,7 +257,7 @@ int16_t FILL_MPC_ADDER_MAP[FILL_PRESSURE_ADDER_MAP_SIZE] = {
 };
 
 
-int16_t M_UPSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap duration)
+const int16_t M_UPSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap duration)
     /*                       0    20    40   60    80  100 <- Pedal % */
     /* < 1000 RPM (0%) */ 2000, 1500, 1000,  750,  500,  500, 
     /* 25% Redline     */ 1000,  900,  800,  600,  475,  450,
@@ -266,7 +266,7 @@ int16_t M_UPSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap 
     /* Redline (100%)  */  450,  425,  400,  375,  350,  350
 };
 
-int16_t M_DOWNSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap duration)
+const int16_t M_DOWNSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap duration)
     /*                       0    20    40   60    80  100 <- Pedal % */
     /* < 1000 RPM (0%) */ 2000, 1750, 1500, 1000,  750,  500, 
     /* 25% Redline     */ 1750, 1500, 1200,  800,  650,  450,
@@ -275,7 +275,7 @@ int16_t M_DOWNSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overla
     /* Redline (100%)  */  800,  750,  500,  500,  400,  300
 };
 
-int16_t S_UPSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap duration)
+const int16_t S_UPSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap duration)
     /*                       0    20    40   60    80  100 <- Pedal % */
     /* < 1000 RPM (0%) */ 2000, 1500, 1000,  750,  500,  500, 
     /* 25% Redline     */ 1000,  900,  800,  600,  475,  450,
@@ -284,7 +284,7 @@ int16_t S_UPSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap 
     /* Redline (100%)  */  450,  425,  400,  375,  350,  350
 };
 
-int16_t S_DOWNSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap duration)
+const int16_t S_DOWNSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap duration)
     /*                       0    20    40   60    80  100 <- Pedal % */
     /* < 1000 RPM (0%) */ 2000, 1750, 1500, 1000,  750,  500, 
     /* 25% Redline     */ 1750, 1500, 1200,  800,  650,  450,
@@ -293,7 +293,7 @@ int16_t S_DOWNSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overla
     /* Redline (100%)  */  800,  750,  500,  500,  400,  300
 };
 
-int16_t A_UPSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap duration)
+const int16_t A_UPSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap duration)
     /*                       0    20    40   60    80  100 <- Pedal % */
     /* < 1000 RPM (0%) */ 2000, 1500, 1000,  750,  500,  500, 
     /* 25% Redline     */ 1000,  900,  800,  600,  475,  450,
@@ -302,7 +302,7 @@ int16_t A_UPSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap 
     /* Redline (100%)  */  450,  425,  400,  375,  350,  350
 };
 
-int16_t A_DOWNSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap duration)
+const int16_t A_DOWNSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap duration)
     /*                       0    20    40   60    80  100 <- Pedal % */
     /* < 1000 RPM (0%) */ 2000, 1750, 1500, 1000,  750,  500, 
     /* 25% Redline     */ 1750, 1500, 1200,  800,  650,  450,
@@ -312,7 +312,7 @@ int16_t A_DOWNSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overla
 };
 
 
-int16_t C_UPSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap duration)
+const int16_t C_UPSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap duration)
     /*                       0    20    40   60    80  100 <- Pedal % */
     /* < 1000 RPM (0%) */ 2000, 1750, 1500, 1250, 1000,  750, 
     /* 25% Redline     */ 1000,  900,  805,  800,  750,  700,
@@ -321,7 +321,7 @@ int16_t C_UPSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap 
     /* Redline (100%)  */  700,  675,  650,  625,  600,  550
 };
 
-int16_t C_DOWNSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap duration)
+const int16_t C_DOWNSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap duration)
     /*                       0    20    40   60    80  100 <- Pedal % */
     /* < 1000 RPM (0%) */ 2000, 1750, 1500, 1250, 1000,  800, 
     /* 25% Redline     */ 1800, 1600, 1400, 1250,  950,  750,
@@ -330,7 +330,7 @@ int16_t C_DOWNSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overla
     /* Redline (100%)  */ 1200, 1000,  900,  800,  700,  600
 };
 
-int16_t W_UPSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap duration)
+const int16_t W_UPSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap duration)
     /*                       0    20    40   60    80  100 <- Pedal % */
     /* < 1000 RPM (0%) */ 2000, 1750, 1500, 1250, 1000,  750, 
     /* 25% Redline     */ 1000,  900,  805,  800,  750,  700,
@@ -339,7 +339,7 @@ int16_t W_UPSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap 
     /* Redline (100%)  */  700,  675,  650,  625,  600,  550
 };
 
-int16_t W_DOWNSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap duration)
+const int16_t W_DOWNSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap duration)
     /*                       0    20    40   60    80  100 <- Pedal % */
     /* < 1000 RPM (0%) */ 2000, 1750, 1500, 1250, 1000,  800, 
     /* 25% Redline     */ 1800, 1600, 1400, 1250,  950,  750,
@@ -348,7 +348,7 @@ int16_t W_DOWNSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overla
     /* Redline (100%)  */ 1200, 1000,  900,  800,  700,  600
 };
 
-int16_t R_UPSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap duration)
+const int16_t R_UPSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap duration)
     /*                       0    20    40   60    80  100 <- Pedal % */
     /* < 1000 RPM (0%) */  100,  100,  100,  100,  100,  100, 
     /* 25% Redline     */  100,  100,  100,  100,  100,  100,
@@ -357,7 +357,7 @@ int16_t R_UPSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap 
     /* Redline (100%)  */  100,  100,  100,  100,  100,  100
 };
 
-int16_t R_DOWNSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap duration)
+const int16_t R_DOWNSHIFT_TIME_MAP[] = { // Value = Target time in ms to shift (overlap duration)
     /*                       0    20    40   60    80  100 <- Pedal % */
     /* < 1000 RPM (0%) */  100,  100,  100, 100,  100,  100, 
     /* 25% Redline     */  100,  100,  100, 100,  100,  100,

e2d36cf20c40810ef6915d04fbc0800061850b26
Ashcon Mohseninia <ashconm@outlook.com>
2023-10-30 16:55:40 +0000

    Avoid jumping MPC

--- src/gearbox.cpp
+++ src/gearbox.cpp
@@ -623,7 +623,7 @@ bool Gearbox::elapse_shift(ProfileGearChange req_lookup, AbstractProfile *profil
 
                 //float sportiness = interpolate_float(sensor_data.driver_requested_torque, 1.0, 0.5, 50, gearboxConfig.max_torque, InterpType::Linear);
                 if (now_cs.off_clutch_speed < 100) {
-                    current_mod_clutch_pressure = interpolate_float(phase_elapsed, spring_pressure_off_clutch, 0, 0, chars.target_shift_time, InterpType::Linear);
+                    current_mod_clutch_pressure = interpolate_float(phase_elapsed, prev_mod_clutch_pressure, 0, 0, chars.target_shift_time, InterpType::Linear);
                 }
                 // Max shift clutch pressure increase beyond shift time (Fixes slow 1-2)
                 //float overlap_ending_spc = current_working_pressure + prefill_data.fill_pressure_on_clutch*1.5;

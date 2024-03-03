e637740617665fb69911cfcd1bbf9a2ff1b97eca
Ashcon Mohseninia <ashconm@outlook.com>
2024-03-03 14:33:37 +0000

    Add EGS Calibration hot reload

--- src/diag/kwp2000.cpp
+++ src/diag/kwp2000.cpp
@@ -13,6 +13,7 @@
 #include "clock.hpp"
 #include "nvs/device_mode.h"
 #include "esp_flash.h"
+#include "egs_calibration/calibration_structs.h"
 
 typedef struct {
     uint8_t day;
@@ -918,6 +919,13 @@ void Kwp2000_server::process_start_routine_by_local_ident(uint8_t* args, uint16_
             } else {
                 // Can only fail if adapt manager is nullptr (Not ready)
                 make_diag_neg_msg(SID_START_ROUTINE_BY_LOCAL_IDENT, NRC_CONDITIONS_NOT_CORRECT_REQ_SEQ_ERROR);
+            }
+        } else if (args[0] == ROUTINE_CALIBRATION_HOT_RELOAD) {
+            esp_err_t res = EGSCal::reload_egs_calibration();
+            if (ESP_OK == res) {
+                make_diag_pos_msg(SID_START_ROUTINE_BY_LOCAL_IDENT, nullptr, 0);
+            } else {
+                make_diag_neg_msg(SID_START_ROUTINE_BY_LOCAL_IDENT, NRC_GENERAL_REJECT);
             }
         } else {
             make_diag_neg_msg(SID_START_ROUTINE_BY_LOCAL_IDENT, NRC_SUB_FUNC_NOT_SUPPORTED_INVALID_FORMAT);
--- src/egs_calibration/calibration.cpp
+++ src/egs_calibration/calibration.cpp
@@ -2,6 +2,7 @@
 #include "tcu_alloc.h"
 #include "esp_flash.h"
 #include "esp_log.h"
+#include "string.h"
 
 CalibrationInfo* CAL_RAM_PTR = NULL;
 HydraulicCalibration* HYDR_PTR = NULL;
@@ -22,7 +23,6 @@ esp_err_t EGSCal::init_egs_calibration() {
     esp_err_t ret = ESP_OK;
     CAL_RAM_PTR = (CalibrationInfo*)TCU_HEAP_ALLOC(sizeof(CalibrationInfo));
     if (NULL == CAL_RAM_PTR) {
-        ESP_LOGE("CAL", "No memory to copy Calibration!");
         ret = ESP_ERR_NO_MEM;
     } else {
         // Allocation OK!
@@ -31,7 +31,6 @@ esp_err_t EGSCal::init_egs_calibration() {
             // Copy OK!
             if (CAL_RAM_PTR->magic != 0xDEADBEEF) {
                 // Magic failed
-                ESP_LOGE("CAL", "Calibration load failed. Magic invalid");
                 ret = ESP_ERR_INVALID_VERSION;
                 goto exit;
             }
@@ -52,12 +51,50 @@ esp_err_t EGSCal::init_egs_calibration() {
             HYDR_PTR = &CAL_RAM_PTR->hydr_cal;
             MECH_PTR = &CAL_RAM_PTR->mech_cal;
             TCC_CFG_PTR = &CAL_RAM_PTR->tcc_cal;
-        } else {
-            ESP_LOGE("CAL", "Copy from flash failed with %s!", esp_err_to_name(ret));
         }
     }
 exit:
     return ret;
 }
 
-// These functions here exist just as a nice way to get the pointer to calibration structures in RAM. They should be optimized out by the compiler //
+esp_err_t EGSCal::reload_egs_calibration() {
+    // First of all, allocate a temp buffer
+    esp_err_t ret = ESP_OK;
+    if (nullptr == CAL_RAM_PTR) { // CAL Info is not set, cannot 'reload', as it was never allocated
+        ret = ESP_ERR_INVALID_STATE;
+    } else {
+        CalibrationInfo* tmp = (CalibrationInfo*)TCU_HEAP_ALLOC(sizeof(CalibrationInfo));
+        if (NULL == tmp) {
+            ret = ESP_ERR_NO_MEM;
+        } else {
+            // Allocation OK!
+            ret = esp_flash_read(NULL, (void*)tmp, CALIBRATION_START_ADDRESS, sizeof(CalibrationInfo));
+            if (ESP_OK == ret) {
+                // Copy OK!
+                if (tmp->magic != 0xDEADBEEF) {
+                    // Magic failed
+                    ret = ESP_ERR_INVALID_VERSION;
+                    goto exit;
+                }
+                if (tmp->len != sizeof(CalibrationInfo)) {
+                    // Size mismatch (Maybe data is added?)
+                    ESP_LOGE("CAL", "Calibration load failed. Length mismatch. Length at info is %d, calibration size is %d", (int)tmp->len, (int)sizeof(CalibrationInfo));
+                    ret = ESP_ERR_INVALID_SIZE;
+                    goto exit;
+                }
+                uint16_t crc_calculated = crc(&((uint8_t*)tmp)[8], sizeof(CalibrationInfo)-8);
+                if (crc_calculated != tmp->crc) {
+                    // CRC Error
+                    ESP_LOGE("CAL", "Calibration load failed. CRC error. Wanted %04X, got %04X", crc_calculated, tmp->crc);
+                    ret = ESP_ERR_INVALID_CRC;
+                    goto exit;
+                }
+                // Copy the temporery CalInfo to the in use one!
+                memcpy(CAL_RAM_PTR, tmp, sizeof(CalibrationInfo));
+            }
+            TCU_FREE(tmp);
+        }
+    }
+exit:
+    return ret;
+}

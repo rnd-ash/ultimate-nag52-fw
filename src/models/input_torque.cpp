0f0fba6058cdca30b4e2c33cdd0696b2912bb143
Ashcon Mohseninia <ashconm@outlook.com>
2024-03-05 08:13:49 +0000

    Fix input torque multiplication

--- src/models/input_torque.cpp
+++ src/models/input_torque.cpp
@@ -19,8 +19,8 @@ int16_t InputTorqueModel::get_input_torque(EgsBaseCan* can_hal, SensorData* meas
         //Interpolate map, but faster, since its just 4 values, no need to allocate a whole map for this
         float x1 = (float)(TCC_CFG_PTR->multiplier_map_x[0]) / 1000.0;
         float x2 = (float)(TCC_CFG_PTR->multiplier_map_x[1]) / 1000.0;
-        float z1 = (float)(TCC_CFG_PTR->multiplier_map_z[0]) / 1000.0;
-        float z2 = (float)(TCC_CFG_PTR->multiplier_map_z[1]) / 1000.0;
+        float z1 = (float)(TCC_CFG_PTR->multiplier_map_z[0]) / 100.0;
+        float z2 = (float)(TCC_CFG_PTR->multiplier_map_z[1]) / 100.0;
         
         float multi = interpolate_float(rpm_multi, z1, z2, x1, x2, InterpType::Linear);
         ret = motor_torque * multi;

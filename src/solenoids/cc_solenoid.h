009e8a8a2e500f6590902b23c13df2cdf8331997
Ashcon Mohseninia <ashconm@outlook.com>
2026-04-08 12:17:54 +0100

    CC Solenoid - overhaul current control with PID algorithm

--- src/solenoids/cc_solenoid.h
+++ src/solenoids/cc_solenoid.h
@@ -4,21 +4,20 @@
 #include "pwm_solenoid.h"
 #include "driver/gptimer.h"
 
-extern float mpc_sol_trim_factor;
-
 class ConstantCurrentSolenoid : public PwmSolenoid {
 public:
-    explicit ConstantCurrentSolenoid(const char *name, ledc_timer_t ledc_timer, gpio_num_t pwm_pin, ledc_channel_t channel, adc_channel_t read_channel, uint16_t phase_duration_ms);
+    explicit ConstantCurrentSolenoid(const char* name, ledc_timer_t ledc_timer, gpio_num_t pwm_pin, ledc_channel_t channel, adc_channel_t read_channel, uint16_t phase_duration_ms);
     void __write_pwm(float vref_compensation, float temperature_factor, bool stop_compensation);
     void set_current_target(uint16_t target_ma);
     void update_when_reading(uint16_t battery);
     uint16_t get_current_target();
     float get_trim();
 private:
-    float internal_trim_factor = 0.0;
+    int16_t trim_pwm = 0;
     uint16_t saved_current_target = 0;
     uint16_t current_target = 0;
     bool correct_cycle = false;
+    int32_t pid[3] = { 0,0,0 };
 };
 
 #endif

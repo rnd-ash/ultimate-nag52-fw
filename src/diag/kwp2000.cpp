d74bb57117da58a210ca5f002ec25961c9df8612
Ashcon Mohseninia <ashconm@outlook.com>
2026-09-06 08:43:24 +0100

    Correct EGS53 diag code

--- src/diag/kwp2000.cpp
+++ src/diag/kwp2000.cpp
@@ -148,7 +148,7 @@ Kwp2000_server::Kwp2000_server(EgsBaseCan* can_layer, Gearbox* gearbox) {
                 this->diag_var_code = 0x0252;
                 break;
             case 3:
-                this->diag_var_code = 0x0353;
+                this->diag_var_code = 0x0253;
                 break;
             default:
                 this->diag_var_code = 0x0000;

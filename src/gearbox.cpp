adc2d89b042a30a83a5c364d2653bb997aa5e695
Ashcon Mohseninia <ashconm@outlook.com>
2026-09-14 10:53:16 +0100

    Fix misuse of shift flags

--- src/gearbox.cpp
+++ src/gearbox.cpp
@@ -442,7 +442,12 @@ bool Gearbox::elapse_shift(GearChange req_lookup, AbstractProfile* profile, bool
         else {
             bool is_release = false;
             if (
-                (sensor_data.converted_driver_torque > threshold_torque || (SHIFT_FLAG_COAST == 0 || SHIFT_FLAG_COAST_54_43 == 0))
+                (sensor_data.converted_driver_torque > threshold_torque || 
+                    (
+                        (sid.shift_flags & SHIFT_FLAG_COAST) == 0 || 
+                        (sid.shift_flags & SHIFT_FLAG_COAST_54_43) == 0
+                    )
+                )
             ) {
                 is_release = true;
             }
